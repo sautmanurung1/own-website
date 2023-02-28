1eba05adc601294269973b9b75bad3ab7fc83d71
sautmanurung1 <smanurung360@gmail.com>
2023-03-01 03:35:20 +0700

    update code about shortener and shorten code

--- shorten.php
+++ shorten.php
@@ -1,25 +1,73 @@
 <?php
 // Connect to the database
-$conn = mysqli_connect('localhost', 'username', 'password', 'database');
+$conn = mysqli_connect('sql102.epizy.com', 'epiz_31064923', 'ynDeteQ0fbL', 'epiz_31064923_url');
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get the URL from the form submission
-$url = $_POST['url'];
+if (isset($_POST['url'])) {
+    // Get the URL from the form submission
+    $url = $_POST['url'];
 
-// Generate a unique short code
-$short_code = generateRandomString(6);
+    // Generate a unique short code
+    $short_code = generateRandomString(6);
 
-// Insert the URL and its short code into the database
-$sql = "INSERT INTO urls (url, short_code) VALUES ('$url', '$short_code')";
-if (mysqli_query($conn, $sql)) {
-    // Display the shortened URL
-    echo "Shortened URL: https://sautmanurung.my.id/$short_code";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    // Insert the URL and its short code into the database
+    $sql = "INSERT INTO urls (url, short_code) VALUES ('$url', '$short_code')";
+    if (mysqli_query($conn, $sql)) {
+        // Construct the short URL
+        $short_url = 'https://sautmanurung.my.id/' . $short_code;
+
+        // Display the shortened URL
+        echo '
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>URL Shortener</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f7f7f7;
+                    }
+                    h1 {
+                        color: #333;
+                        text-align: center;
+                        margin-top: 50px;
+                    }
+                    p {
+                        text-align: center;
+                        margin-top: 50px;
+                        font-size: 24px;
+                    }
+                    a {
+                        display: block;
+                        text-align: center;
+                        margin-top: 20px;
+                        padding: 10px;
+                        border-radius: 5px;
+                        border: none;
+                        background-color: #333;
+                        color: #fff;
+                        font-size: 16px;
+                        text-decoration: none;
+                    }
+                    a:hover {
+                        background-color: #555;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>URL Shortener</h1>
+                <p>Your short URL:</p>
+                <a href="' . $short_url . '">' . $short_url . '</a>
+            </body>
+            </html>
+        ';
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 }
 
 // Close the database connection
--- shortener.php
+++ shortener.php
@@ -1,15 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>URL Shortener</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+        }
+        h1 {
+            color: #333;
+            text-align: center;
+            margin-top: 50px;
+        }
+        form {
+            width: 400px;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+        }
+        input[type=text] {
+            display: block;
+            width: 95%;
+            margin-bottom: 10px;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            font-size: 16px;
+        }
+        input[type=submit] {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            background-color: #333;
+            color: #fff;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        input[type=submit]:hover {
+            background-color: #555;
+        }
+    </style>
 </head>
 <body>
     <h1>URL Shortener</h1>
     <form method="POST" action="shorten.php">
-        <input type="text" name="url">
+        <input type="text" name="url" placeholder="Enter your URL here...">
         <input type="submit" value="Shorten">
     </form>
 </body>
